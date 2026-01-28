b158854fc5075d4e83781232f12c607fba155799
Anton <chechulinanton899@gmail.com>
2026-01-28 17:13:54 +0200

    feat: add brain-prime game
    - Add prime.js game logic with finding a prime number
    - Register new CLI entry point in package.json bin
    - Update Makefile with new game commands
    - Extend general game logic to support new game type
    - Add brain-prime to available games

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import runGame from "./general_logic_game.js";
+
+const isPrimeNum = (num) => {
+    if (num < 2) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+
+    const centerPointNum = Math.sqrt(num);
+    for (let i = 3; i <= centerPointNum; i += 2) {
+        if (num % i === 0) {
+            return false;
+        }
+        return true;
+    }
+};
+const getRound = () => {
+    const randomNum = Math.floor(Math.random() * 101);
+    const question = String(randomNum);
+    const correctAnswer = isPrimeNum(randomNum) ? "yes" : "no";
+    return { question, correctAnswer };
+};
+
+const playPrimeGame = () => {
+    const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    runGame(description, getRound);
+};
+
+export default playPrimeGame;
